b11a8531389519b31d6c58838373c52ddb3b8f10
Jack Lloyd <jack@randombit.net>
2018-08-05 18:15:56 -0400

    Document Version

--- botan/src/version.rs
+++ botan/src/version.rs
@@ -4,16 +4,25 @@ use botan_sys::*;
 use std::ffi::CStr;
 
 #[derive(Debug)]
+/// Information about the library version
 pub struct Version {
+    /// The major version of the library
     pub major: u32,
+    /// The minor version of the library
     pub minor: u32,
+    /// The patch version of the library
     pub patch: u32,
+    /// The release date of the library, as YYYYMMDD, for example
+    /// 2.7.0 has value 20180702. Will be 0 for unreleased versions.
     pub release_date: u32,
+    /// The version of the FFI API, as a YYYYMMDD field.
     pub ffi_api: u32,
+    /// A free-form string describing the library version
     pub string: String,
 }
 
 impl Version {
+    /// Read the version information
     pub fn new() -> Version {
 
         unsafe {
