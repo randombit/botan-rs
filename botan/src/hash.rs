0aa239a1156ee339b9c5ad06e8b5b96d685dc728
Jack Lloyd <jack@randombit.net>
2018-08-05 18:12:55 -0400

    Document hash and MAC types

--- botan/src/hash.rs
+++ botan/src/hash.rs
@@ -6,6 +6,7 @@ use std::ffi::CString;
 use std::ptr;
 
 #[derive(Debug)]
+/// A hash function object
 pub struct HashFunction {
     obj: botan_hash_t,
     output_length: usize
@@ -24,6 +25,15 @@ impl Drop for HashFunction {
 }
 
 impl HashFunction {
+    /// Create a new hash function
+    ///
+    /// # Errors
+    /// Will fail if the named hash is not known
+    /// # Examples
+    /// ```
+    /// assert!(botan::HashFunction::new("SHA-256").is_ok());
+    /// assert!(botan::HashFunction::new("Hash9000").is_err());
+    /// ```
     pub fn new(name: &str) -> Result<HashFunction> {
         let mut obj = ptr::null_mut();
         call_botan! { botan_hash_init(&mut obj, CString::new(name).unwrap().as_ptr(), 0u32) };
@@ -36,27 +46,70 @@ impl HashFunction {
 
     // FIXME(2.8) need name getters
 
-    pub fn duplicate(&self) -> Result<HashFunction> {
-        let mut obj = ptr::null_mut();
-        call_botan! { botan_hash_copy_state(&mut obj, self.obj) };
-        Ok(HashFunction { obj: obj, output_length: self.output_length })
-    }
-
+    /// Return the output length of the hash function, in bytes
+    ///
+    /// # Examples
+    /// ```
+    /// let hash = botan::HashFunction::new("SHA-256").unwrap();
+    /// assert_eq!(hash.output_length(), 32);
+    /// ```
     pub fn output_length(&self) -> usize { self.output_length }
 
+    /// Add data to a hash computation, may be called many times
+    ///
+    /// # Examples
+    /// ```
+    /// let hash = botan::HashFunction::new("SHA-256").unwrap();
+    /// hash.update(&[1,2,3]).unwrap();
+    /// hash.update(&[4,5,6]).unwrap();
+    /// ```
     pub fn update(&self, data: &[u8]) -> Result<()> {
         call_botan! { botan_hash_update(self.obj, data.as_ptr(), data.len()) };
         Ok(())
     }
 
+    /// Finalize the computation, returning the hash of the message
+    ///
+    /// # Examples
+    /// ```
+    /// let hash = botan::HashFunction::new("SHA-256").unwrap();
+    /// hash.update(&[1,2,3]).unwrap();
+    /// hash.update(&[4,5,6]).unwrap();
+    /// let digest = hash.finish().unwrap();
+    /// ```
     pub fn finish(&self) -> Result<Vec<u8>> {
         let mut output = vec![0; self.output_length()];
         call_botan! { botan_hash_final(self.obj, output.as_mut_ptr()) };
         Ok(output)
     }
 
+    /// Clear the internal state of the hash function. It acts as if it
+    /// was newly created, and is ready to compute a new digest.
+    /// Basically the same as calling final, but without returning a
+    /// result.
     pub fn clear(&self) -> Result<()> {
         call_botan! { botan_hash_clear(self.obj) };
         Ok(())
+    }
+
+    /// Copy hash object state to a new object, allowing prefixes of
+    /// messages to be hashed. This function is also called by clone.
+    ///
+    /// # Errors
+    /// Should not fail but might due to unexpected error
+    /// # Examples
+    /// ```
+    /// let hash = botan::HashFunction::new("SHA-256").unwrap();
+    /// hash.update(&[1,2,3]);
+    /// let hash2 = hash.duplicate().unwrap();
+    /// hash2.update(&[4,5,6]);
+    /// let result1 = hash.finish().unwrap(); // hash of 1,2,3
+    /// let result2 = hash2.finish().unwrap(); // hash of 1,2,3,4,5,6
+    /// ```
+
+    pub fn duplicate(&self) -> Result<HashFunction> {
+        let mut obj = ptr::null_mut();
+        call_botan! { botan_hash_copy_state(&mut obj, self.obj) };
+        Ok(HashFunction { obj: obj, output_length: self.output_length })
     }
 }
--- botan/src/mac.rs
+++ botan/src/mac.rs
@@ -6,6 +6,7 @@ use std::ffi::CString;
 use std::ptr;
 
 #[derive(Debug)]
+// Message authentication code
 pub struct MsgAuthCode {
     obj: botan_mac_t,
     output_length: usize
@@ -18,6 +19,15 @@ impl Drop for MsgAuthCode {
 }
 
 impl MsgAuthCode {
+    /// Create a new message authentication code
+    ///
+    /// # Examples
+    /// ```
+    /// let hmac = botan::MsgAuthCode::new("HMAC(SHA-256)").unwrap();
+    /// ```
+    /// ```
+    /// let poly1305 = botan::MsgAuthCode::new("Poly1305").unwrap();
+    /// ```
     pub fn new(name: &str) -> Result<MsgAuthCode> {
         let mut obj = ptr::null_mut();
         call_botan! { botan_mac_init(&mut obj, CString::new(name).unwrap().as_ptr(), 0u32) };
@@ -30,24 +40,68 @@ impl MsgAuthCode {
 
     // FIXME(2.8) need name and key length info getters
 
+    /// Return the output length of the authentication code, in bytes
+    /// # Examples
+    /// ```
+    /// let hmac = botan::MsgAuthCode::new("HMAC(SHA-256)").unwrap();
+    /// assert_eq!(hmac.output_length(), 32);
+    /// ```
     pub fn output_length(&self) -> usize { self.output_length }
 
+    /// Set the key for the authentication code object
+    /// # Examples
+    /// ```
+    /// let hmac = botan::MsgAuthCode::new("HMAC(SHA-256)").unwrap();
+    /// hmac.set_key(&vec![0; 16]).unwrap();
+    /// ```
     pub fn set_key(&self, key: &[u8]) -> Result<()> {
         call_botan! { botan_mac_set_key(self.obj, key.as_ptr(), key.len()) };
         Ok(())
     }
 
+    /// Add data to a MAC computation, may be called many times
+    ///
+    /// # Examples
+    /// ```
+    /// let hmac = botan::MsgAuthCode::new("HMAC(SHA-256)").unwrap();
+    /// assert!(hmac.update(&[23]).is_err()); // key not set yet
+    /// hmac.set_key(&vec![0; 16]).unwrap();
+    /// hmac.update(&[1,2,3]).unwrap();
+    /// hmac.update(&[4,5,6]).unwrap();
+    /// ```
     pub fn update(&self, data: &[u8]) -> Result<()> {
         call_botan! { botan_mac_update(self.obj, data.as_ptr(), data.len()) };
         Ok(())
     }
 
+    /// Complete a MAC computation, after which the object is reset to
+    /// MAC a new message with the same key.
+    ///
+    /// # Examples
+    /// ```
+    /// let hmac = botan::MsgAuthCode::new("HMAC(SHA-256)").unwrap();
+    /// assert!(hmac.update(&[23]).is_err()); // key not set yet
+    /// hmac.set_key(&vec![0; 16]).unwrap();
+    /// hmac.update(&[1,2,3]).unwrap();
+    /// hmac.update(&[4,5,6]).unwrap();
+    /// let mac = hmac.finish().unwrap();
+    /// ```
     pub fn finish(&self) -> Result<Vec<u8>> {
         let mut output = vec![0; self.output_length()];
         call_botan! { botan_mac_final(self.obj, output.as_mut_ptr()) };
         Ok(output)
     }
 
+    /// Clear the MAC key
+    ///
+    /// # Examples
+    /// ```
+    /// let hmac = botan::MsgAuthCode::new("HMAC(SHA-256)").unwrap();
+    /// hmac.set_key(&vec![0; 16]).unwrap();
+    /// hmac.update(&[1,2,3]).unwrap();
+    /// hmac.clear().unwrap();
+    /// assert!(hmac.update(&[23]).is_err()); // key not set anymore
+    /// ```
     pub fn clear(&self) -> Result<()> {
         call_botan! { botan_mac_clear(self.obj) };
         Ok(())
