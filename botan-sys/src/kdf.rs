4d19eed6469c3e64d6dc9cef3ea90c7507ca583b
Jack Lloyd <jack@randombit.net>
2018-09-13 18:28:09 -0400

    Add decls for new pwdhash functions

--- botan-sys/src/kdf.rs
+++ botan-sys/src/kdf.rs
@@ -10,15 +10,6 @@ extern "C" {
                        salt_len: usize,
                        iterations: usize) -> c_int;
 
-    pub fn botan_scrypt(out: *mut u8,
-                        out_len: usize,
-                        passphrase: *const c_char,
-                        salt: *const u8,
-                        salt_len: usize,
-                        N: usize,
-                        r: usize,
-                        p: usize) -> c_int;
-
     pub fn botan_pbkdf_timed(pbkdf_algo: *const c_char,
                              out: *mut u8,
                              out_len: usize,
@@ -27,6 +18,38 @@ extern "C" {
                              salt_len: usize,
                              milliseconds_to_run: usize,
                              out_iterations_used: *mut usize) -> c_int;
+
+    pub fn botan_pwdhash(algo: *const c_char,
+                         param1: usize,
+                         param2: usize,
+                         param3: usize,
+                         out: *mut u8,
+                         out_len: usize,
+                         passphrase: *const c_char,
+                         passphrase_len: usize,
+                         salt: *const u8,
+                         salt_len: usize) -> c_int;
+
+    pub fn botan_pwdhash_timed(algo: *const c_char,
+                               msec: u32,
+                               param1: *mut usize,
+                               param2: *mut usize,
+                               param3: *mut usize,
+                               out: *mut u8,
+                               out_len: usize,
+                               passphrase: *const c_char,
+                               passphrase_len: usize,
+                               salt: *const u8,
+                               salt_len: usize);
+
+    pub fn botan_scrypt(out: *mut u8,
+                        out_len: usize,
+                        passphrase: *const c_char,
+                        salt: *const u8,
+                        salt_len: usize,
+                        N: usize,
+                        r: usize,
+                        p: usize) -> c_int;
 
     pub fn botan_kdf(kdf_algo: *const c_char,
                      out: *mut u8,
