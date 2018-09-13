c0dbde22b441a1cec9a21d7f9da93aa183ce68b0
Jack Lloyd <jack@randombit.net>
2018-09-13 18:29:01 -0400

    Use new pwdhash functions

--- botan/src/pbkdf.rs
+++ botan/src/pbkdf.rs
@@ -4,6 +4,45 @@ use utils::*;
 
 /// Password based key derivation function
 ///
+/// # Examples
+/// ```
+/// let rng = botan::RandomNumberGenerator::new().unwrap();
+/// let salt = rng.read(10).unwrap();
+/// let key = botan::derive_key_from_password("Scrypt", 32, "passphrase", &salt, 8192, 8, 1).unwrap();
+/// assert_eq!(key.len(), 32);
+/// ```
+pub fn derive_key_from_password(
+    algo: &str,
+    out_len: usize,
+    passphrase: &str,
+    salt: &[u8],
+    param1: usize,
+    param2: usize,
+    param3: usize) -> Result<Vec<u8>> {
+
+    let algo = make_cstr(algo)?;
+    let passphrase = make_cstr(passphrase)?;
+
+    let mut output = vec![0u8; out_len];
+
+    call_botan! {
+        botan_pwdhash(algo.as_ptr(),
+                      param1,
+                      param2,
+                      param3,
+                      output.as_mut_ptr(),
+                      output.len(),
+                      passphrase.as_ptr(),
+                      0,
+                      salt.as_ptr(),
+                      salt.len())
+    }
+
+    Ok(output)
+}
+
+/// Password based key derivation function
+///
 /// Note currently only PBKDF2 is supported by this interface.
 /// For PBKDF2, iterations >= 100000 is recommended.
 ///
@@ -20,22 +59,8 @@ pub fn pbkdf(algo: &str,
              salt: &[u8],
              iterations: usize) -> Result<Vec<u8>> {
 
-    let algo = make_cstr(algo)?;
-    let passphrase = make_cstr(passphrase)?;
-
-    let mut output = vec![0u8; out_len];
-
-    call_botan! { botan_pbkdf(algo.as_ptr(),
-                              output.as_mut_ptr(),
-                              output.len(),
-                              passphrase.as_ptr(),
-                              salt.as_ptr(),
-                              salt.len(),
-                              iterations) }
-
-    Ok(output)
+    derive_key_from_password(algo, out_len, passphrase, salt, iterations, 0, 0)
 }
-
 
 /// Scrypt key derivation
 ///
@@ -59,17 +84,5 @@ pub fn scrypt(out_len: usize,
               r: usize,
               p: usize) -> Result<Vec<u8>> {
 
-    let passphrase = make_cstr(passphrase)?;
-
-    let mut output = vec![0u8; out_len];
-
-    call_botan! {
-        botan_scrypt(output.as_mut_ptr(), output.len(),
-                     passphrase.as_ptr(),
-                     salt.as_ptr(), salt.len(),
-                     n, r, p)
-    }
-
-    Ok(output)
-
+    derive_key_from_password("Scrypt", out_len, passphrase, salt, n, r, p)
 }
