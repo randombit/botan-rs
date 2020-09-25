a2257fceb784b3b09ad3d15eaf569cae590d5358
Jack Lloyd <jack@randombit.net>
2020-09-25 17:09:17 -0400

    Run clippy in CI

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,6 +3,11 @@
 if [ "x$FEATURES" = "x" ]; then
     cargo build --verbose
     cargo test --verbose
+
+    if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
+        cargo clippy
+    fi
+
 else
     cd botan-sys
     cargo build --verbose
